e1d4225cb1f86aec54b1821c0be29f887ad89738
tushar-raval <tushar.raval.2782@gmail.com>
2021-08-19 09:02:21 +0530

    [ADD]added templates and connected

--- dental_app/urls.py
+++ dental_app/urls.py
@@ -4,4 +4,23 @@ from .views import *
 
 urlpatterns = [
     path('base_html/',base_html, name="base"),
+    path('',index_html, name="index"),
+    path('replaceing_html/',replacing_html,name="replace"),
+    path('root_canal_html/',root_canal_html,name="canal"),
+    path('wisdom_teeth_html/',wisdom_teeth_html,name="wisdom"),
+    path('dental_implants_html/',dental_implants_html,name="implants"),
+    path('correction_crooked_html/',correction_crooked_html,name="crooked"),
+    path('cavities_html/',cavities_html,name="cavities"),
+    path('toothache_html/',toothache_html,name="toothache"),
+    path('bad_breath_html/',bad_breath_html,name="bad_breath"),
+    path('sensitivity_html/',sensitivity_html,name="sensitivity"),
+    path('broken_teeth_html/',broken_teeth_html,name="broken_teeth"),
+    path('loose_teeth_html/',loose_teeth_html,name="loose_teeth"),
+    path('short_teeth_html/',short_teeth_html,name="short_teeth"),
+    path('bleeding_gums_html/',bleeding_gums_html,name="bleeding_gums"),
+    path('crooked_teeth_html/',crooked_teeth_html,name="crooked_teeth"),
+    path('about_doctor_html/',about_doctor_html,name="doctor"),
+    path('about_clinic_html/',about_clinic_html,name="clinic"),
     ]
+
+    
--- dental_app/views.py
+++ dental_app/views.py
@@ -5,3 +5,54 @@ from django.views.generic import View, ListView, UpdateView, DeleteView
 
 def base_html(request):
     return render(request,'base.html')
+
+def index_html(request):
+    return render(request,'index.html')
+    
+def replacing_html(request):
+    return render(request,'replacing_missing_teeth.html')
+
+def root_canal_html(request):
+    return render(request,'root_canal_treatment.html')
+
+def wisdom_teeth_html(request):
+    return render(request,'wisdom_teeth.html')
+
+def dental_implants_html(request):
+    return render(request,'dental_implants.html')
+
+def correction_crooked_html(request):
+    return render(request,'correction_crooked.html')
+
+def cavities_html(request):
+    return render(request,'cavities.html')
+
+def toothache_html(request):
+    return render(request,'toothache.html')
+
+def bad_breath_html(request):
+    return render(request,'bad_breath.html')
+
+def sensitivity_html(request):
+    return render(request,'sensitivity.html')
+
+def broken_teeth_html(request):
+    return render(request,'broken_teeth.html')
+
+def loose_teeth_html(request):
+    return render(request,'loose_teeth.html')
+
+def short_teeth_html(request):
+    return render(request,'short_teeth.html')
+
+def bleeding_gums_html(request):
+    return render(request,'bleeding_gums.html')
+
+def crooked_teeth_html(request):
+    return render(request,'crooked_teeth.html')
+
+def about_doctor_html(request):
+    return render(request,'about_the_doctor.html')
+
+def about_clinic_html(request):
+    return render(request,'about_the_clinic.html')
